70fdea150686ff209a9e64a6f781dada597eeab8
nosami <jasonimison@gmail.com>
2016-09-16 14:20:36 +0100

    make XF template work

--- main/external/fsharpbinding/MonoDevelop.FSharp.Tests/ProjectTemplates.fs
+++ main/external/fsharpbinding/MonoDevelop.FSharp.Tests/ProjectTemplates.fs
@@ -4,10 +4,12 @@ open System.IO
 open System.Reflection
 open System.Runtime.CompilerServices
 open System.Threading.Tasks
+open Microsoft.FSharp.Compiler.SourceCodeServices
 open Mono.Addins
 open MonoDevelop.Core
 open MonoDevelop.Core.Instrumentation
 open MonoDevelop.Core.ProgressMonitoring
+open MonoDevelop.FSharp
 open MonoDevelop.Ide
 open MonoDevelop.Ide.Projects
 open MonoDevelop.Ide.Templates
@@ -37,7 +39,6 @@ type ``Template tests``() =
     let predicate = new Predicate<SolutionTemplate>(templateMatch)
 
     let rec flattenCategories (category: TemplateCategory) = 
-        printfn "%A" category.Id
         seq {
             yield category
             yield! category.Categories |> Seq.collect flattenCategories
@@ -58,7 +59,8 @@ type ``Template tests``() =
     let profileMap =
         Map.ofList [".NETPortable", "portable-net45+netcore45+wp8"
                     ".NETFramework", "net40"
-                    "MonoAndroid" , "portable-net45+netcore45+wp8"]
+                    "MonoAndroid", "portable-net45+netcore45+wp8"
+                    "Xamarin.iOS", "Xamarin.iOS10"]
 
     member x.Templates =
         solutionTemplates |> Seq.map (fun t -> t.Id)
@@ -67,12 +69,16 @@ type ``Template tests``() =
     [<TestCaseSource ("Templates")>]
     member x.``Build every template`` (tt:string) =
         toTask <| async {
-            let template = ProjectTemplate.ProjectTemplates |> Seq.find (fun t -> t.Id == tt)
-            let dir = FilePath (FilePath(".").FullPath.ToString() / "buildtemplates" / template.Id)
+            let projectTemplate = ProjectTemplate.ProjectTemplates |> Seq.find (fun t -> t.Id == tt)
+            let dir = FilePath (FilePath(".").FullPath.ToString() / "buildtemplates" / projectTemplate.Id)
             dir.Delete()
             Directory.CreateDirectory (dir |> string) |> ignore
             let cinfo = new ProjectCreateInformation (ProjectBasePath = dir, ProjectName = tt, SolutionName = tt, SolutionPath = dir)
             cinfo.Parameters.["CreateSharedAssetsProject"] <- "False"
+            cinfo.Parameters.["CreatePortableDotNetProject"] <- "True"
+            cinfo.Parameters.["CreateMonoTouchProject"] <- "True"
+            cinfo.Parameters.["UseXamarinAndroidSupportv7AppCompat"] <- "True"
+            cinfo.Parameters.["CreateAndroidProject"] <- "True"
             cinfo.Parameters.["UseUniversal"] <- "True"
             cinfo.Parameters.["UseIPad"] <- "False"
             cinfo.Parameters.["UseIPhone"] <- "False"
@@ -83,16 +89,18 @@ type ``Template tests``() =
             cinfo.Parameters.["AndroidMinSdkVersionAttribute"] <- "android:minSdkVersion=\"10\""
             cinfo.Parameters.["AndroidThemeAttribute"] <- ""
 
-            let sln = template.CreateWorkspaceItem (cinfo) :?> Solution
+            let sln = projectTemplate.CreateWorkspaceItem (cinfo) :?> Solution
+            let createTemplate (template:SolutionTemplate) =
+                let config = NewProjectConfiguration(
+                                CreateSolution = false,
+                                ProjectName = tt,
+                                SolutionName = tt,
+                                Location = (dir |> string)
+                             )
+                
+                templateService.ProcessTemplate(template, config, sln.RootFolder)
 
-            let getPackages (template:SolutionTemplate) =
-                let config = NewProjectConfiguration()
-                config.CreateSolution <- true
-                config.ProjectName <- tt
-                config.SolutionName <- tt
-                config.Location <- dir |> string
-
-                let processedTemplate = templateService.ProcessTemplate(template, config, sln.RootFolder)
+            let getPackages (processedTemplate:ProcessedTemplateResult) =
                 let res = 
                     packageInstallers
                     |> Seq.collect(fun _ -> processedTemplate.PackageReferences)
@@ -105,34 +113,64 @@ type ``Template tests``() =
                 solutionTemplates 
                 |> Seq.find(fun t -> t.Id = tt)
 
-            let project = sln.FindProjectByName(tt) :?> DotNetProject
+            //let projects = sln.FindProjectByName(tt) :?> DotNetProject
             let packagesFolder = (dir |> string)/".."/".."/".."/".."/".."/".."/".."/"packages"
+            let projects = sln.Items |> Seq.filter(fun i -> i :? DotNetProject) |> Seq.cast<DotNetProject> |> List.ofSeq
 
-            let assemblyReferencesForPackage (package: ProjectTemplatePackageReference) =
-                let profile = profileMap.[project.TargetFramework.Id.Identifier]
-                let nameAndVersion = sprintf "%s.%s" package.Id package.Version
-                let foldersToScan = [packagesFolder/nameAndVersion/"lib"/"portable-win+net45+wp80+win81+wpa81+MonoAndroid10+MonoTouch10+Xamarin.iOS10" //XF
-                                     packagesFolder/nameAndVersion/"lib"/profile
-                                     packagesFolder/nameAndVersion/"lib"]
+            let fixReferences (project:DotNetProject) =
+                let assemblyReferencesForPackage (package: ProjectTemplatePackageReference) =
+                    let profile = profileMap.[project.TargetFramework.Id.Identifier]
+                    let nameAndVersion = sprintf "%s.%s" package.Id package.Version
+                    let foldersToScan = [packagesFolder/nameAndVersion/"lib"/profile
+                                         packagesFolder/nameAndVersion/"lib"/"portable-win+net45+wp80+win81+wpa81+MonoAndroid10+MonoTouch10+Xamarin.iOS10" //XF
+                                         packagesFolder/nameAndVersion/"lib"]
 
-                let baseFolder = foldersToScan |> List.find Directory.Exists
+                    let baseFolder = foldersToScan |> List.find Directory.Exists
 
-                Directory.GetFiles(baseFolder, "*.dll", SearchOption.AllDirectories)
-                |> Array.map(fun file -> ProjectReference.CreateCustomReference(ReferenceType.Assembly, package.Id, file))
+                    let files = Directory.GetFiles(baseFolder, "*.dll", SearchOption.AllDirectories)
+                    files
+                    |> Array.map(fun file -> ProjectReference.CreateCustomReference(ReferenceType.Assembly, package.Id, file))
 
-            let refs = 
-                getPackages solutionTemplate
-                |> Seq.collect assemblyReferencesForPackage
-                |> Seq.distinct
+                let packages = 
+                    projectTemplate.PackageReferencesForCreatedProjects
+                    |> Seq.tryFind(fun ref -> ref.ProjectName = project.Name)
 
-            for ref in refs do
-                project.References.Add ref
 
+                let addRefs (packageReferences:PackageReferencesForCreatedProject) =
+                    let refs =
+                        packageReferences.PackageReferences
+                        |> Seq.collect assemblyReferencesForPackage
+
+                    for ref in refs do
+                        project.References.Add ref
+
+                packages |> Option.iter addRefs
+
+            for project in projects do
+                fixReferences project
             do! sln.SaveAsync(monitor) |> Async.AwaitTask
 
-            let! result = sln.Build(monitor, sln.DefaultConfigurationSelector, null) |> Async.AwaitTask
-            match result.HasWarnings, result.HasErrors with
-            | _, false -> Assert.Pass()
-            | _ -> 
-                Assert.Fail(sprintf "Build Failed\n%s\n%A" result.CompilerOutput result.Errors)
+            let getErrorsForProject (projects: DotNetProject list) =
+                asyncSeq {
+                    let! result = sln.Build(monitor, sln.DefaultConfigurationSelector, null) |> Async.AwaitTask
+
+                    match tt, result.HasWarnings, result.HasErrors with
+                    | "Xamarin.tvOS.FSharp.SingleViewApp", _, false //MTOUCH : warning MT0094: Both profiling (--profiling) and incremental builds (--fastdev) is not supported when building for tvOS. Incremental builds have ben disabled.]
+                    | _, false, false ->
+                        // xbuild worked, now check for editor squiggles
+                        for project in projects do
+                            let checker = FSharpChecker.Create()
+                            let projectOptions = languageService.GetProjectOptionsFromProjectFile project
+                            let! checkResult = checker.ParseAndCheckProject projectOptions
+                            for error in checkResult.Errors do
+                                yield "Editor error", error.FileName, error.Message
+                    | _ ->
+                        for error in result.Errors do
+                            yield "Build error", error.FileName, error.ErrorText
+                }
+
+            let errors = getErrorsForProject projects |> AsyncSeq.toSeq |> List.ofSeq
+            match errors with
+            | [] -> Assert.Pass()
+            | errors -> Assert.Fail (sprintf "%A" errors)
         }
